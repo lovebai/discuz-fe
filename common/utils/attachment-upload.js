b5e235a7498c26d0944a0947dcf7f04dcf65d671
joneeeeli <joneeeeli@tencent.com>
2021-08-04 18:45:44 +0800

    fix

--- common/utils/attachment-upload.js
+++ common/utils/attachment-upload.js
@@ -1,5 +1,7 @@
 import { createAttachment, getCosTmpKey, updateAttachment } from '@common/server';
-import { fixImageOrientation } from '@common/utils/exif';
+// import { fixImageOrientation } from '@common/utils/exif';
+// import COS from 'cos-wx-sdk-v5';
+// const COS = require('cos-wx-sdk-v5');
 
 
 // type: 1是图片，2是文件
@@ -10,72 +12,76 @@ const attachmentUploadOne = async (file, type = 1) => {
   const Bucket = 'discuz-service-test-1258344699';
   const Region = 'ap-guangzhou';
   const fileName = file.name;
-  const key = `${new Date().getTime()}${fileName}`;
+  const key = `public/attachments/2021/08/04/${new Date().getTime()}${fileName}`;
 
 
 
-  // ----- 小程序cos上传 - begin -----
-  return new Promise(async (resolve) => {
-    const COS = (await import('cos-wx-sdk-v5')).default;
-    const cos = new COS({
-      getAuthorization: async (_options, callback) => {
-        const res = await getCosTmpKey({
-          type,
-          nameArr: [key]
-        });
-        const { code, data } = res;
-        if (code === 0) {
-          cosKeyData = data;
-          const credentials = data && data.credentials;
-          callback({
-            TmpSecretId: credentials.tmpSecretId,
-            TmpSecretKey: credentials.tmpSecretKey,
-            SecurityToken: credentials.sessionToken,
-            StartTime: data.startTime,
-            ExpiredTime: data.expiredTime,
-          });
-        } else {
-          // todo: 处理异常情况，获取临时密钥失败
-        }
-      }
-    });
-    debugger;
-    // 开始上传
-    cos.putObject({
-      Bucket: Bucket,
-      Region: Region,
-      Key: key,
-      FilePath: file,
-      onProgress: function(progressData) {
-          console.log(JSON.stringify(progressData));
-          // todo: 上传进度处理
-      }
-    }, function(err, data) {
-      if (err === null) {
-        const url = cos.getObjectUrl({
-          Bucket: Bucket,
-          Region: Region,
-          Key: key,
-          Sign: false
-        });
+  // // ----- 小程序cos上传 - begin -----
+  // // return new Promise(async (resolve) => {
+  //   // const COS = (await import('cos-wx-sdk-v5')).default;
+  //   debugger;
+  //   const cos = new COS({
+  //     SecretId: 'AKIDCJAnwjKjthEk6HBm6fwzhCLFRRBlsBxG',
+  //     SecretKey: 'DF1SjllSwlna8i9D6jAGFPPzT6TXHnac',
+  //     // getAuthorization: async (_options, callback) => {
+  //     //   debugger;
+  //     //   const res = await getCosTmpKey({
+  //     //     type,
+  //     //     nameArr: [key]
+  //     //   });
+  //     //   const { code, data } = res;
+  //     //   if (code === 0) {
+  //     //     cosKeyData = data;
+  //     //     const credentials = data && data.credentials;
+  //     //     callback({
+  //     //       TmpSecretId: credentials.tmpSecretId,
+  //     //       TmpSecretKey: credentials.tmpSecretKey,
+  //     //       SecurityToken: credentials.sessionToken,
+  //     //       StartTime: data.startTime,
+  //     //       ExpiredTime: data.expiredTime,
+  //     //     });
+  //     //   } else {
+  //     //     // todo: 处理异常情况，获取临时密钥失败
+  //     //   }
+  //     // }
+  //   });
 
-        updateAttachment({
-          type,
-          filePath: url,
-          fileName,
-          fileSize: file.size,
-          fileType: file.type,
-          requestId: cosKeyData?.requestId,
-        });
+  //   // 开始上传
+  //   cos.putObject({
+  //     Bucket: Bucket,
+  //     Region: Region,
+  //     Key: key,
+  //     FilePath: file,
+  //     onProgress: function(progressData) {
+  //         console.log(JSON.stringify(progressData));
+  //         // todo: 上传进度处理
+  //     }
+  //   }, function(err, data) {
+  //     if (err === null) {
+  //       const url = cos.getObjectUrl({
+  //         Bucket: Bucket,
+  //         Region: Region,
+  //         Key: key,
+  //         Sign: false
+  //       });
 
-        resolve(url);
+  //       updateAttachment({
+  //         type,
+  //         filePath: url,
+  //         fileName,
+  //         fileSize: file.size,
+  //         fileType: file.type,
+  //         requestId: cosKeyData?.requestId,
+  //       });
 
-      } else if (err) {
-        // todo: 异常情况处理，上传失败
-      }
-    });
-  });
-  // ----- 小程序cos上传 - end -----
+  //       resolve(url);
+
+  //     } else if (err) {
+  //       // todo: 异常情况处理，上传失败
+  //     }
+  //   });
+  // // });
+  // // ----- 小程序cos上传 - end -----
 
 
 
