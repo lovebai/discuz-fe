c32459fc9d2f6aaad8598ff5a661a49129d466cc
Sean Luo <luoxinran@gmail.com>
2021-08-09 16:26:53 +0800

    话题页面缓存恢复

--- web/layout/search/result-topic/h5/index.jsx
+++ web/layout/search/result-topic/h5/index.jsx
@@ -12,10 +12,10 @@ class SearchResultTopicH5Page extends React.Component {
   constructor(props) {
     super(props);
 
-    const keyword = this.props.router.query.keyword || '';
+    const keyword = this.props.search.currentTopicKeyword || '';
 
     this.state = {
-      keyword,
+      keyword: keyword,
       refreshing: false,
     };
   }
@@ -39,6 +39,7 @@ class SearchResultTopicH5Page extends React.Component {
   };
 
   onSearch = (keyword) => {
+    this.props.search.currentTopicKeyword = keyword;
     this.setState({ keyword }, () => {
       this.refreshData();
     });
