5607113f49201ba724f7f9a7c7b96806400d9319
jinyuuzhang <jinyuuzhang@tencent.com>
2021-08-10 19:30:09 +0800

    完成h5前端二维码兜底

--- web/layout/card/footer/index.jsx
+++ web/layout/card/footer/index.jsx
@@ -2,21 +2,24 @@ import React, { useEffect, useState } from 'react';
 import { inject, observer } from 'mobx-react';
 import { getMiniCode } from '@server';
 import styles from './index.module.scss';
+import QRCode from 'qrcode.react';
+
 const Index = ({ site, setReady, threadId = '' }) => {
   const [miniCode, setMiniCode] = useState('');
+  const [miniConfig, setMiniConfig] = useState(true);
   const defaultLogo = '/dzq-img/default-logo.png';
   const { siteName } = site.webConfig?.setSite || '';
+  const threadPath = `/indexPages/thread/index?id=${threadId}`;
+  const sitePath = '/indexPages/index/index';
+  const path = threadId ? threadPath : sitePath;
+  const paramPath = `/pages/index/index?path=${encodeURIComponent(path)}`;
   useEffect(async () => {
     try {
-      const threadPath = `/indexPages/thread/index?id=${threadId}`;
-      const sitePath = '/indexPages/index/index';
-      const path = threadId ? threadPath : sitePath;
-      const paramPath = `/pages/index/index?path=${encodeURIComponent(path)}`;
       const res = await getMiniCode({ params: { path: paramPath } });
       if (res?.code === 0) {
         setMiniCode(res?.data.base64Img);
       } else {
-        setMiniCode(defaultLogo);
+        setMiniConfig(false);
       }
     } catch {
       setMiniCode(defaultLogo);
@@ -25,9 +28,11 @@ const Index = ({ site, setReady, threadId = '' }) => {
   }, []);
   return (
         <div className={styles.footerBox}>
-            <img src={miniCode} className={styles.footerImg}/>
+            {!miniConfig
+              ? <QRCode value={paramPath} size={96}></QRCode>
+              : <img src={miniCode} className={styles.footerImg}/>}
             <span className={styles.desc}>
-                长按识别小程序查看详情
+                {`长按识别${miniConfig ? '小程序' : '二维码'}查看详情`}
             </span>
             <span className={styles.siteName}>
                 {`来自${siteName}`}
