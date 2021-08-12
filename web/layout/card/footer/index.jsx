9fff6145f1a3de379ce901ba2e246861ab23013c
jinyuuzhang <jinyuuzhang@tencent.com>
2021-08-12 10:57:02 +0800

    完成h5二维码兜底功能

--- web/layout/card/footer/index.jsx
+++ web/layout/card/footer/index.jsx
@@ -9,17 +9,21 @@ const Index = ({ site, setReady, threadId = '' }) => {
   const [miniConfig, setMiniConfig] = useState(true);
   const defaultLogo = '/dzq-img/default-logo.png';
   const { siteName } = site.webConfig?.setSite || '';
-  const threadPath = `/indexPages/thread/index?id=${threadId}`;
-  const sitePath = '/indexPages/index/index';
-  const path = threadId ? threadPath : sitePath;
-  const paramPath = `/pages/index/index?path=${encodeURIComponent(path)}`;
+  const href = window.location.href.split('/');
+  href[href.length - 1] = threadId ? `thread/${threadId}` : '';
   useEffect(async () => {
     try {
+      const threadPath = `/indexPages/thread/index?id=${threadId}`;
+      const sitePath = '/indexPages/index/index';
+      const path = threadId ? threadPath : sitePath;
+      const paramPath = `/pages/index/index?path=${encodeURIComponent(path)}`;
       const res = await getMiniCode({ params: { path: paramPath } });
       if (res?.code === 0) {
         setMiniCode(res?.data.base64Img);
-      } else {
+      } else if (res?.code === -7081) {
         setMiniConfig(false);
+      } else {
+        setMiniCode(defaultLogo);
       }
     } catch {
       setMiniCode(defaultLogo);
@@ -29,7 +33,7 @@ const Index = ({ site, setReady, threadId = '' }) => {
   return (
         <div className={styles.footerBox}>
             {!miniConfig
-              ? <QRCode value={paramPath} size={96}></QRCode>
+              ? <QRCode value={href.join('/')} size={96}></QRCode>
               : <img src={miniCode} className={styles.footerImg}/>}
             <span className={styles.desc}>
                 {`长按识别${miniConfig ? '小程序' : '二维码'}查看详情`}
