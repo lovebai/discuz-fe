85d8c80b83e1cbe47aa6549fff4c11b61bb35c79
Sean Luo <luoxinran@gmail.com>
2021-08-13 18:10:03 +0800

    减少返回热门后的请求次数

--- web/layout/search/result-topic/h5/index.jsx
+++ web/layout/search/result-topic/h5/index.jsx
@@ -57,14 +57,11 @@ class SearchResultTopicH5Page extends React.Component {
     // 当服务器无法获取数据时，触发浏览器渲染
     const hasTopics = !!search.topics;
 
-    if (!hasTopics || (keyword && keyword !== search.currentTopicKeyword)) { // 缓存为空或者url参数与缓存不同，刷新
+    if (!hasTopics || (keyword && keyword !== search.currentTopicKeyword)) { // 缓存为空或者url参数与缓存不同，刷新；否则，有缓存的关键词，需要回到原来位置
       this.page = 1;
       search.resetResultData();
       baselayout.resultTopic = -1;
       await search.getTopicsList({ search: keyword, perPage: this.perPage });
-    } else if (!search.currentTopicKeyword) { // 有缓存的关键词，需要回到原来位置
-      this.page = 1;
-      await search.getTopicsList({ search: search.currentTopicKeyword, perPage: this.perPage, hasTopics: hasTopics });
     }
   }
 
