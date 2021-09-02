c6adf12cef3fd94e1e524a8ec77c5b573d0a3d1d
angelzou <angelzou@tencent.com>
2021-09-02 15:37:44 +0800

    fix: 修复请求参数名称

--- common/utils/api-statistic-params/index.js
+++ common/utils/api-statistic-params/index.js
@@ -6,6 +6,6 @@ export const setStatisticParams = (config) => {
   const ua = isServer() ? (config.headers['user-agent'] || '') : window.navigator.userAgent;
   const serverId = getServerId();
   const platfrom = getPlatform(ua);
-  config.params = { ...config.params, dzqFid: serverId, dzqPf: platfrom };
+  config.params = { ...config.params, dzqSid: serverId, dzqPf: platfrom };
   return config;
 };
