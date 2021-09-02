befd5f84bf5b8b05b69231b02f0bb3ef136a4339
VS-ZX <2417818740@qq.com>
2021-09-02 17:24:43 +0800

    fix: 3192 【小程序-消息-测试环境】通过PC端发送网站代码生成的音频与视频导致，小程序的消息页异常，点击进入消息页会直接跳转到优酷视频云页面，无法直接看到整个消息页面

--- mini/src/components/message/notice-item/index.jsx
+++ mini/src/components/message/notice-item/index.jsx
@@ -101,7 +101,7 @@ class Index extends Component {
       _content = tip + _content;
     }
 
-    let t = xss(s9e.parse(this.filterTag(_content)));
+    let t = xss(s9e.parseEmoji(this.filterTag(_content)));
     t = (typeof t === 'string') ? t : '';
 
     return t;
@@ -146,7 +146,7 @@ class Index extends Component {
   }
 
   render() {
-    const { type, item = {}, isLast } = this.props;
+    const { type, item = { }, isLast } = this.props;
     const avatarUrl = this.getAvatar(item.avatar);
 
     return (
@@ -231,7 +231,7 @@ Index.propTypes = {
 
 Index.defaultProps = {
   type: 'account',
-  item: {},
+  item: { },
 }
 
 export default Index;
