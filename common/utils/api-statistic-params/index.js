aa74e6ee9dc2d713558adc3d0b45be625e358148
angelzou <angelzou@tencent.com>
2021-09-02 14:58:11 +0800

    feat: api请求添加上报数据的参数

---
+++ common/utils/api-statistic-params/index.js
@@ -0,0 +1,11 @@
+import { getServerId } from './get-server-id';
+import getPlatform from './get-platform';
+import isServer from '@common/utils/is-server';
+
+export const setStatisticParams = (config) => {
+  const ua = isServer() ? (config.headers['user-agent'] || '') : window.navigator.userAgent;
+  const serverId = getServerId();
+  const platfrom = getPlatform(ua);
+  config.params = { ...config.params, dzqFid: serverId, dzqPf: platfrom };
+  return config;
+};
