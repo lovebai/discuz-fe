ad91b99209f957f84e41898127a5550b88a5a4e3
angelzou <angelzou@tencent.com>
2021-09-17 12:27:00 +0800

    feat: web端报名帖数据同步

--- common/plugin/custom-apply/client/display/adapter/web/index.jsx
+++ common/plugin/custom-apply/client/display/adapter/web/index.jsx
@@ -1,21 +1,52 @@
 import React from 'react';
-import { inject, observer } from 'mobx-react';
+import { withRouter } from 'next/router';
+import { inject } from 'mobx-react';
 import { Button, Icon, Avatar, Toast } from '@discuzq/design';
 import { createRegister } from '@discuzq/sdk/dist/api/plugin/create-register';
 import { deleteRegister } from '@discuzq/sdk/dist/api/plugin/delete-register';
 import PopupList from '@components/thread/popup-list';
+import CountDown from '@common/utils/count-down';
+import { debounce } from '@common/utils/throttle-debounce';
+import LoginHelper from '@common/utils/login-helper';
 import classNames from 'classnames';
 import styles from '../index.module.scss';
 
+let countDownIns = null;
 @inject('thread')
 @inject('index')
+@inject('user')
 class CustomApplyDisplay extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       popupShow: false,
       loading: false,
+      minutes: 0,
+      seconds: 0,
+      isApplyEnd: false, // 报名时间是否已结束
     };
+  }
+
+  componentDidMount() {
+    if (!countDownIns) countDownIns = new CountDown();
+    const { renderData } = this.props;
+    const { body } = renderData || {};
+    if (body?.registerEndTime) {
+      const time = body?.registerEndTime?.replace(/-/g, '/');
+      countDownIns.start(time, (res) => {
+        const { days, hours, minutes, seconds } = res;
+        const ms = (days * 24 * 60) + (hours * 60) + minutes;
+        this.setState({ minutes: ms, seconds });
+        if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
+          this.setState({ isApplyEnd: true });
+          countDownIns?.stop();
+        }
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    if (!countDownIns) countDownIns?.stop();
   }
 
   getUserCls = (users = []) => {
@@ -26,55 +57,79 @@ class CustomApplyDisplay extends React.Component {
 
   getActStatusText = () => {
     const { body } = this.props.renderData || {};
+    const { isApplyEnd } = this.state;
     if (body?.isExpired) return '活动已过期';
     if (body?.isMemberFull) return '人数已满';
+    if (isApplyEnd) return '报名已结束';
   };
 
   handleActOperate = async () => {
-    const { renderData, thread, index, siteData } = this.props;
+    const { renderData, thread, index, siteData, user } = this.props;
+    if (!user.isLogin()) {
+      LoginHelper.saveAndLogin();
+      return;
+    }
     const { tomId, body } = renderData || {};
-    const { isRegistered, activityId, registerUsers } = body;
+    const { isRegistered, activityId, registerUsers, totalNumber } = body;
     const action = isRegistered ? deleteRegister : createRegister;
     this.setState({ loading: true });
     const res = await action({ data: { activityId } });
     this.setState({ loading: false });
     if (res.code === 0) {
       let authorInfo = {};
+      // 是否是详情页
       if (siteData.isDetailPage) {
         authorInfo = thread.authorInfo;
       } else {
-        const { data = {} } = index._findAssignThread(siteData.threadId);
-        authorInfo = data?.user;
+        const { data } = index._findAssignThread(siteData?.threadId) || {};
+        authorInfo = data?.user || {};
       }
       const uid = authorInfo.id || authorInfo.userId;
       const users = registerUsers.filter(item => item.userId !== uid);
-      if (!isRegistered) users.push({
-        userId: uid,
-        avatar: authorInfo.avatarUrl || authorInfo.avatar,
-        nickname: authorInfo.nickname,
-      });
+      let currentNumber = body?.currentNumber - 1;
+      // 没有报名
+      if (!isRegistered) {
+        users.unshift({
+          userId: uid,
+          avatar: authorInfo.avatarUrl || authorInfo.avatar,
+          nickname: authorInfo.nickname,
+        });
+        currentNumber = body?.currentNumber + 1;
+      }
+      const tid = siteData.isDetailPage ? thread?.threadData?.id : siteData?.threadId;
       const tomValue = {
         body: {
           ...body,
           isRegistered: !isRegistered,
           registerUsers: users,
+          currentNumber,
+          isMemberFull: currentNumber === totalNumber,
         },
         tomId,
+        threadId: tid,
       };
       thread.updateThread(tomId, tomValue);
-      const threadData = index.updateListThreadIndexes(thread?.threadData?.id, tomId, tomValue);
+      const threadData = index.updateListThreadIndexes(tid, tomId, tomValue);
       if (threadData && siteData.recomputeRowHeights) siteData.recomputeRowHeights(threadData);
       Toast.info({ content: isRegistered ? '取消报名成功' : '报名成功' });
     } else Toast.error({ content: res.msg || '报名失败' });
     return res;
   };
 
+  handleMoreClick = () => {
+    const { siteData } = this.props;
+    if (siteData?.threadId) {
+      this.props.router.push(`/thread/${siteData?.threadId}`);
+    }
+  };
+
   render() {
     const { siteData, renderData } = this.props;
+    const { isApplyEnd, minutes, seconds } = this.state;
     if (!renderData) return null;
     const { body } = renderData || {};
     const { isRegistered } = body;
-    const isCanNotApply = body?.isExpired || body?.isMemberFull;
+    const isCanNotApply = body?.isExpired || body?.isMemberFull || isApplyEnd;
     const { popupShow } = this.state;
     const platform = siteData.platform === 'h5' ? styles.h5 : styles.pc;
     return (
@@ -85,7 +140,7 @@ class CustomApplyDisplay extends React.Component {
               {body?.title && body?.title}
             </div>
             <div className={styles['wrapper-header__right']}>
-              还有<span className={styles['text-primary']}>14</span>分<span className={styles['text-primary']}>13</span>秒结束报名
+              还有<span className={styles['text-primary']}>{minutes}</span>分<span className={styles['text-primary']}>{seconds}</span>秒结束报名
             </div>
           </div>
           <div className={styles['wrapper-content']}>
@@ -98,7 +153,12 @@ class CustomApplyDisplay extends React.Component {
                 }>
                   {body?.content}
                 </div>
-                {!siteData?.isDetailPage && (<div className={classNames(styles['text-primary'], styles.more)}>查看详情 ></div>)}
+                {!siteData?.isDetailPage && (
+                  <div
+                    className={classNames(styles['text-primary'], styles.more)}
+                    onClick={this.handleMoreClick}
+                  >查看详情 ></div>
+                )}
               </>
             )}
             <div className={classNames(styles['wrapper-content__tip'], styles.mt4n)}>
@@ -134,7 +194,7 @@ class CustomApplyDisplay extends React.Component {
                     if (index <= 2) return <Avatar circle={true} size="small" text={item.nickname} image={item.avatar} key={item.nickname} />;
                     return null;
                   })}
-                  <span className={styles.m10}>{(body?.registerUsers || []).length}人已报名</span>
+                  <span className={styles.m10}>{body?.currentNumber}人已报名</span>
                 </div>
                 <Button
                   type="primary"
@@ -163,4 +223,4 @@ class CustomApplyDisplay extends React.Component {
   }
 };
 
-export default CustomApplyDisplay;
+export default withRouter(CustomApplyDisplay);
--- common/store/index/action.js
+++ common/store/index/action.js
@@ -316,7 +316,7 @@ class IndexAction extends IndexStore {
     if (this.threads) {
       const { pageData = [] } = this.threads;
       for (let i = 0; i < pageData.length; i++) {
-        if (pageData[i].threadId === threadId) {
+        if (+pageData[i].threadId === +threadId) {
           return { index: i, data: pageData[i] };
         }
       }
