d1c3b0080972cfbe374e07fc66928e2e0d5fa3b5
zonewellhe(贺重威) <zonewellhe@tencent.com>
2021-09-22 16:52:25 +0800

    fixbug

--- web/layout/thread/pc/components/comment-input/index.jsx
+++ web/layout/thread/pc/components/comment-input/index.jsx
@@ -206,7 +206,7 @@ const CommentInput = inject('site')((props) => {
 
   const onFail = (ret) => {
     const msg = ret?.msg;
-    const code = ret?.code === -7075; // 错误码为-7075时为不允许上传敏感图
+    const code = ret?.code === -7075 || ret?.code === -4002; // 错误码为-7075时为不允许上传敏感图
     Toast.error({
       content: code ? msg : '图片上传失败',
     });
