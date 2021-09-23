74acc0890a146b516e6cddeeedc1fd530b910b7e
angelzou <angelzou@tencent.com>
2021-09-23 17:09:45 +0800

    fix: 移动端报错问题

--- web/layout/thread/h5/content/index.jsx
+++ web/layout/thread/h5/content/index.jsx
@@ -280,7 +280,7 @@ const RenderThreadContent = inject('site', 'user')(observer((props) => {
         )}
 
           {
-            DZQPluginCenter.injection('plugin_detail', 'thread_extension_display_hook').map(({render, pluginInfo}) => {
+            DZQPluginCenter && DZQPluginCenter.injection('plugin_detail', 'thread_extension_display_hook').map(({render, pluginInfo}) => {
               return (
                 <div key={pluginInfo.name}>
                   {render({
