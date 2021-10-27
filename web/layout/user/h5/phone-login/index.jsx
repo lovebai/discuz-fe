8188e292385c66df90dde3ac537d08ddcb2c9d96
kevinchou <kevinchou@tencent.com>
2021-10-27 16:15:05 +0800

    fix: 修改pc头部插件异常

--- web/layout/user/h5/phone-login/index.jsx
+++ web/layout/user/h5/phone-login/index.jsx
@@ -175,7 +175,7 @@ class LoginPhoneH5Page extends React.Component {
         {
           platform === 'h5'
             ? <HomeHeader hideInfo mode='login'/>
-            : <Header/>
+            : null
         }
         <div className={platform === 'h5' ? layout.content : layout.pc_content}>
           <div className={platform === 'h5' ? layout.title : layout.pc_title}>手机号码登录/注册</div>
