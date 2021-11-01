3100e219eca134526d194f3f3014d6880615c6f0
LamHo <linhao.4zhong@163.com>
2021-11-01 15:48:04 +0800

    修改名字

--- common/plugin/custom-index-banner/View/src/replace/adapter/index.js
+++ common/plugin/custom-index-banner/View/src/replace/adapter/index.js
@@ -1,6 +1,6 @@
 let pluginComponent = () => {};
 if (process.env.DISCUZ_ENV === 'mini') {
-  pluginComponent = require('./mini/index.jsx');
+  pluginComponent = require('./mini/index.jsx.js');
 }
 if (process.env.DISCUZ_ENV === 'web') {
   pluginComponent = require('./web/index.jsx');
