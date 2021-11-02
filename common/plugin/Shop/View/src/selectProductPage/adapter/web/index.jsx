ead9aa3cb59f1b70437884df9459ab4b92ad92bc
fishcui <fishcui@tencent.com>
2021-11-02 16:21:48 +0800

    fix: 修复微信小商店文字最大长度的问题 & 最大选中数量的问题

--- common/plugin/Shop/View/src/selectProductPage/adapter/web/index.jsx
+++ common/plugin/Shop/View/src/selectProductPage/adapter/web/index.jsx
@@ -205,16 +205,17 @@ export default class SelectProduct extends React.PureComponent {
   handleProductSelected = (checkedStatus, productInfo) => {
     const { productId } = productInfo;
 
-    if (Object.keys(this.state.selectedMiniShopProducts).length > 10) {
-      Toast.error({
-        content: '最多只能同时选择十条小商店商品'
-      })
-      return;
-    }
-
     const nextSelectedStatus = Object.assign({}, this.state.selectedMiniShopProducts);
 
     if (checkedStatus) {
+      if (Object.keys(this.state.selectedMiniShopProducts).length >= 10) {
+        Toast.error({
+          content: '最多只能同时选择十条小商店商品'
+        })
+        return;
+      }
+      
+
       nextSelectedStatus[productId] = productInfo;
     } else {
       delete nextSelectedStatus[productId];
