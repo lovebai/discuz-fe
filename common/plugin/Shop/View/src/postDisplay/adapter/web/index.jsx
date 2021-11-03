b48906c9d07619cff6d1329cbeee9abfdaa3daba
fishcui <fishcui@tencent.com>
2021-11-03 16:13:47 +0800

    chore: 删除无用的 log

--- common/plugin/Shop/View/src/postDisplay/adapter/web/index.jsx
+++ common/plugin/Shop/View/src/postDisplay/adapter/web/index.jsx
@@ -131,7 +131,6 @@ export default class ShopCreateDisplay extends React.Component {
 
   render() {
     const { renderData } = this.props;
-    console.log(renderData);
     if (!renderData) return null;
 
     const { body = { products: [] } } = renderData;
