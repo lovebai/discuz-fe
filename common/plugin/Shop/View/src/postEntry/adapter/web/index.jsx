f9ae160d68e4f70f2fda0b9ccb8bb3af36fa7cfc
fishcui <fishcui@tencent.com>
2021-11-03 15:45:56 +0800

    fix: 修复 pc 选择可以超过十条的问题

--- common/plugin/Shop/View/src/postEntry/adapter/web/index.jsx
+++ common/plugin/Shop/View/src/postEntry/adapter/web/index.jsx
@@ -290,16 +290,16 @@ export default class ShopPostEntry extends React.Component {
   handleProductSelected = (checkedStatus, productInfo) => {
     const { productId } = productInfo;
 
-    if (Object.keys(this.state.selectedMiniShopProducts).length >= 10) {
-      Toast.error({
-        content: '最多只能同时选择十条小商店商品'
-      })
-      return;
-    }
-
     const nextSelectedStatus = Object.assign({}, this.state.selectedMiniShopProducts);
 
     if (checkedStatus) {
+      if (Object.keys(this.state.selectedMiniShopProducts).length >= 10) {
+        Toast.error({
+          content: '最多只能同时选择十条小商店商品'
+        })
+        return;
+      }
+      
       nextSelectedStatus[productId] = productInfo;
     } else {
       delete nextSelectedStatus[productId];
