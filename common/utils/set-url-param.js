62fbc5700567eda5c8455d78e016ebe2fbff64a7
v_zjxzzheng <v_zjxzzheng@tencent.com>
2021-11-08 17:17:39 +0800

    feat: web分享添加邀请码

--- common/utils/set-url-param.js
+++ common/utils/set-url-param.js
@@ -1,11 +1,15 @@
 export default function setUrlParam(url = '', params = {}) {
   const isCarryParam = url.includes('?');
   let newUrl = url;
-  Object.keys(params).forEach((v, i) => {
-    if (!params[v]) {
-      return;
-    }
-    newUrl += `${!isCarryParam && i === 0 ? '?' : '&'}${v}=${params[v]}`;
-  });
-  return newUrl;
+  try {
+    Object.keys(params).forEach((v, i) => {
+      if (!params[v]) {
+        return;
+      }
+      newUrl += `${!isCarryParam && i === 0 ? '?' : '&'}${v}=${params[v]}`;
+    });
+    return newUrl;
+  } catch (e) {
+    return '';
+  }
 }
