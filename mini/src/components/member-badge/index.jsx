51a8b0ee16b76cf237bfeed1feccde92154f6097
fayelliao <fayelliao@tencent.com>
2021-11-10 14:33:14 +0800

    feat:小程序更换线上链接

--- mini/src/components/member-badge/index.jsx
+++ mini/src/components/member-badge/index.jsx
@@ -5,11 +5,11 @@ import { IMG_SRC_HOST } from '@common/constants/site';
 import { View, Text, Image } from '@tarojs/components';
 
 // 部署后替换为线上链接
-import badgeImg_1 from '../../../../web/public/dzq-img/member-badge_1.png';
-import badgeImg_2 from '../../../../web/public/dzq-img/member-badge_2.png';
-import badgeImg_3 from '../../../../web/public/dzq-img/member-badge_3.png';
-import badgeImg_4 from '../../../../web/public/dzq-img/member-badge_4.png';
-import badgeImg_5 from '../../../../web/public/dzq-img/member-badge_5.png';
+// import badgeImg_1 from '../../../../web/public/dzq-img/member-badge_1.png';
+// import badgeImg_2 from '../../../../web/public/dzq-img/member-badge_2.png';
+// import badgeImg_3 from '../../../../web/public/dzq-img/member-badge_3.png';
+// import badgeImg_4 from '../../../../web/public/dzq-img/member-badge_4.png';
+// import badgeImg_5 from '../../../../web/public/dzq-img/member-badge_5.png';
 export default class MemberBadge extends React.Component {
   constructor(props) {
     super(props);
@@ -17,20 +17,20 @@ export default class MemberBadge extends React.Component {
   }
   getMemberBadgeIcon() {
     const { groupLevel } = this.props;
-    switch (groupLevel) {
-      case 1:
-        return badgeImg_1;
-      case 2:
-        return badgeImg_2;
-      case 3:
-        return badgeImg_3;
-      case 4:
-        return badgeImg_4;
-      case 5:
-        return badgeImg_5;
-    }
+    // switch (groupLevel) {
+    //   case 1:
+    //     return badgeImg_1;
+    //   case 2:
+    //     return badgeImg_2;
+    //   case 3:
+    //     return badgeImg_3;
+    //   case 4:
+    //     return badgeImg_4;
+    //   case 5:
+    //     return badgeImg_5;
+    // }
 
-    // return `${IMG_SRC_HOST}/assets/member-badge_${groupLevel}.png`
+    return `${IMG_SRC_HOST}/dzq-img/member-badge_${groupLevel}.png`
   }
 
   render() {
