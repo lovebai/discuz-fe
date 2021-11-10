8362259b42d6c4311a6a7ead92c548f8eb575fb5
v_zjxzzheng <v_zjxzzheng@tencent.com>
2021-11-10 10:02:08 +0800

    feat: 分享添加微博

--- common/utils/set-wx-share.js
+++ common/utils/set-wx-share.js
@@ -10,7 +10,8 @@ export default function setWxShare(title, desc, link, img, inviteCode = '') {
             link: setUrlParam(link || window.location.href, { inviteCode }), // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
             imgUrl: img, // 分享图标
         };
-        wx.updateAppMessageShareData(dataInfo); // 分享给朋友
-        wx.updateTimelineShareData(dataInfo); // 分享到朋友圈
+        wx.updateAppMessageShareData(dataInfo); // 分享给朋友/QQ
+        wx.updateTimelineShareData(dataInfo); // 分享到朋友圈/QQ空间
+        wx.onMenuShareWeibo(dataInfo); // 分享到微博
     });
 }
