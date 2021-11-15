8270511931119f454bd29096e194c2e116f8a73f
fishcui <fishcui@tencent.com>
2021-11-15 15:59:49 +0800

    fix: 变更部分付费引用 web 路径

--- web/components/thread-post/payment/part-paid/index.jsx
+++ web/components/thread-post/payment/part-paid/index.jsx
@@ -3,12 +3,12 @@
  * @author fishcui
  */
 import React from 'react';
-import TextUnit from '@components/thread/all-post-paid/part-paid/units/textUnit';
-import ImageUnit from '@components/thread/all-post-paid/part-paid/units/imageUnit';
-import VideoUnit from '@components/thread/all-post-paid/part-paid/units/videoUnit';
-import AudioUnit from '@components/thread/all-post-paid/part-paid/units/audioUnit';
-import AttachmentUnit from '@components/thread/all-post-paid/part-paid/units/attachmentUnit';
-import PriceUnit from '@components/thread/all-post-paid/part-paid/units/priceUnit';
+import TextUnit from '@components/thread-post/payment/part-paid/units/textUnit';
+import ImageUnit from '@components/thread-post/payment/part-paid/units/imageUnit';
+import VideoUnit from '@components/thread-post/payment/part-paid/units/videoUnit';
+import AudioUnit from '@components/thread-post/payment/part-paid/units/audioUnit';
+import AttachmentUnit from '@components/thread-post/payment/part-paid/units/attachmentUnit';
+import PriceUnit from '@components/thread-post/payment/part-paid/units/priceUnit';
 
 class PartPaid extends React.Component {
   render() {
