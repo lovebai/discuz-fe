505bce9019b0661a900e1173859a0dbeaa1e7ef5
lovebai <1507820372@qq.com>
2023-04-03 08:35:52 +0800

    优化

--- web/pages/_document.jsx
+++ web/pages/_document.jsx
@@ -14,49 +14,51 @@ class MyDocument extends Document {
     if (process.env.NODE_ENV === 'development') {
       return '';
     }
-    return `
-      setTimeout(function() {
-        var appid = '';
-        var channelname = window.location.hostname;
-        if (!!window.navigator.userAgent.match(/AppleWebKit.*Mobile.*/)) {
-          appid = '500D36509CE649E88446FB4E7A51B221'; // h5
-        } else {
-          appid = '4F323A1D5F444BF69C7C4E10704AEA2F'; // pc
-        }
-        var url = 'http://sdk.talkingdata.com/app/h5/v1?appid=' + appid + '&vn=' + '${currEnvConfig.version}' + '&vc=' + '${currEnvConfig.version}' + '&td_channelid=' + channelname;
-        if ( window.location.protocol.indexOf('https') != -1 ) {
-          url = 'https://jic.talkingdata.com/app/h5/v1?appid=' + appid + '&vn=' + '${currEnvConfig.version}' + '&vc=' + '${currEnvConfig.version}' + '&td_channelid=' + channelname;
-        }
-        var talkingdata = document.createElement('script');
-        talkingdata.type = 'text/javascript';
-        talkingdata.async = true;
-        talkingdata.src = url;
-        document.getElementsByTagName('body')[0].appendChild(talkingdata);
+    // return `
+    //   setTimeout(function() {
+    //     var appid = '';
+    //     var channelname = window.location.hostname;
+    //     if (!!window.navigator.userAgent.match(/AppleWebKit.*Mobile.*/)) {
+    //       appid = '500D36509CE649E88446FB4E7A51B221'; // h5
+    //     } else {
+    //       appid = '4F323A1D5F444BF69C7C4E10704AEA2F'; // pc
+    //     }
+    //     var url = 'http://sdk.talkingdata.com/app/h5/v1?appid=' + appid + '&vn=' + '${currEnvConfig.version}' + '&vc=' + '${currEnvConfig.version}' + '&td_channelid=' + channelname;
+    //     if ( window.location.protocol.indexOf('https') != -1 ) {
+    //       url = 'https://jic.talkingdata.com/app/h5/v1?appid=' + appid + '&vn=' + '${currEnvConfig.version}' + '&vc=' + '${currEnvConfig.version}' + '&td_channelid=' + channelname;
+    //     }
+    //     var talkingdata = document.createElement('script');
+    //     talkingdata.type = 'text/javascript';
+    //     talkingdata.async = true;
+    //     talkingdata.src = url;
+    //     document.getElementsByTagName('body')[0].appendChild(talkingdata);
 
+    //     window.sessionStorage.setItem('__TD_td_channel', window.location.hostname.replace(/\./g, '_'));
+    //     var tdjs = document.createElement('script');
+    //     tdjs.type = 'text/javascript';
+    //     tdjs.async = true;
+    //     tdjs.src = 'https://jic.talkingdata.com/app/h5/v1?appid=750AEE91CF4446A19A2D12D5EE32F725';
+    //     document.getElementsByTagName('body')[0].appendChild(tdjs);
 
-        window.sessionStorage.setItem('__TD_td_channel', window.location.hostname.replace(/\./g, '_'));
-        var tdjs = document.createElement('script');
-        tdjs.type = 'text/javascript';
-        tdjs.async = true;
-        tdjs.src = 'https://jic.talkingdata.com/app/h5/v1?appid=750AEE91CF4446A19A2D12D5EE32F725';
-        document.getElementsByTagName('body')[0].appendChild(tdjs);
-
-        var dzqjs = document.createElement('script');
-        dzqjs.type = 'text/javascript';
-        dzqjs.async = true;
-        dzqjs.src = 'https://dl.discuz.chat/dzq.js';
-        document.getElementsByTagName('body')[0].appendChild(dzqjs);
-      }, 2000);
-    `;
+    //     var dzqjs = document.createElement('script');
+    //     dzqjs.type = 'text/javascript';
+    //     dzqjs.async = true;
+    //     dzqjs.src = 'https://dl.discuz.chat/dzq.js';
+    //     document.getElementsByTagName('body')[0].appendChild(dzqjs);
+    //   }, 2000);
+    // `;
+    return '';
   }
 
   render() {
     return (
       <Html lang="en">
         <Head>
-          <meta name="screen-orientation" content="portrait"/>
-          <meta name="x5-orientation" content="portrait"/>
-          <script dangerouslySetInnerHTML={{ __html: `
+          <meta name="screen-orientation" content="portrait" />
+          <meta name="x5-orientation" content="portrait" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
               setTimeout(function() {
                 function remCalc (){
                   var a = 375;
@@ -69,26 +71,31 @@ class MyDocument extends Document {
                 remCalc();
                 window.addEventListener('resize', remCalc);
               }, 0);
-          ` }} />
+          `,
+            }}
+          />
         </Head>
 
-
         <body>
-            <Main />
-            <script dangerouslySetInnerHTML={{ __html: `
+          <Main />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
                 var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
                 var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; //判断是否IE<11浏览器
                 var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
                 if(isIE || isIE11) {
                   document.body.innerHTML = '<style>html,body{width: 100%;height: 100%;display: block;}h1{padding: 20px;}h3{padding: 15px;}.box{text-align: center;margin-top: 40vh;}</style><div class="box"><h1>站点不支持IE浏览器！</h1><h3>请使用QQ浏览器、chrome，Edge等浏览器。</h3></div>';
                 }
-            ` }}>
-
-            </script>
-            <NextScript/>
+            `,
+            }}
+          ></script>
+          <NextScript />
         </body>
-        <script dangerouslySetInnerHTML={{ __html: this.createTalkingdata() }}/>
-        <script dangerouslySetInnerHTML={{ __html: `
+        <script dangerouslySetInnerHTML={{ __html: this.createTalkingdata() }} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
             // 微信设置字体最大，布局乱的补丁
             function is_weixn() {
               var ua = navigator.userAgent.toLowerCase();
@@ -117,9 +124,13 @@ class MyDocument extends Document {
                 });
               }
             }
-        ` }}/>
+        `,
+          }}
+        />
 
-        <script dangerouslySetInnerHTML={{ __html: `
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
             setTimeout(function() {
               // 腾讯地图定位组件
               var geolocation = document.createElement('script');
@@ -136,7 +147,9 @@ class MyDocument extends Document {
               document.getElementsByTagName('body')[0].appendChild(cosDocumentPreviewSDK);
 
             }, 500)
-        ` }}/>
+        `,
+          }}
+        />
       </Html>
     );
   }
